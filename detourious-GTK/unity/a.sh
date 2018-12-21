e6cd588d0d387eeb627ad4d162ce0a4327d22615
Robert Wiley <ylee@bodhilinux.com>
2018-12-21 16:20:47 -0500

    Fix symlinks lintian doesn't like.

---
+++ detourious-GTK/unity/a.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ln -fs close.svg close_focused_normal.svg
+ln -fs close_focused_pressed.svg close_unfocused_pressed.svg
+ln -fs maximize.svg maximize_focused_normal.svg
+ln -fs maximize_focused_prelight.svg maximize_unfocused_prelight.svg
+ln -fs maximize_focused_pressed.svg maximize_unfocused_pressed.svg
+ln -fs minimize.svg minimize_focused_normal.svg
+ln -fs minimize_focused_pressed.svg minimize_unfocused_prelight.svg
+ln -fs minimize_focused_pressed.svg minimize_unfocused_pressed.svg
+ln -fs close_focused_prelight.svg close_unfocused_prelight.svg
